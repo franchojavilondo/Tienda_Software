64cd1610ef1b3cacfde442a4ae72bc4ec826c6ea
franchojavilondo <franchojavilondo@gmail.com>
2014-11-12 16:57:20 +0100

    Cambios varios en busqueda y estilos para ajustar

--- busqueda/busqueda.php
+++ busqueda/busqueda.php
@@ -1,58 +1,25 @@
-<?php 
-session_start();
-
-if (isset($_SESSION["carro"]))
-$carro = $_SESSION["carro"];
-
-else $_SESSION['carro']=array(); 
-
-
-//array_push($_SESSION['carro'],'apple','mango','banana');
-//echo $_SESSION['carro'][8];
-?>
-
 <script type="text/javascript">
-
-
-var carro = <?php 
-if (isset($_SESSION["carro"]))
-echo json_encode($_SESSION["carro"]);
-else 
-echo "[]";
-?>;
-
-
-function carrito(x){
+var carro = [];
+function carrito(x,precio){
+var cadena="Articulos del carrito:\n",i;
 alert("Articulo metido al carrito");
-
-<?php
-$mivarPHP=
-    '<script type="text/javascript">;
-    document.writeln (x.toString());
-</script>';
-
-
-array_push($_SESSION['carro'],$mivarPHP);
-?>
-
-
+carro.push(x);
+carro.push(precio);
+for(i=0;i<carro.length;i++){
+cadena+="ID: "+carro[i]+" PRECIO: "+carro[i+1]+"\n";
+i++;
+}
+alert(cadena);
 var columnas=2;
 crear_filas("tabla_carro",carro,columnas)
-
-
 }
-
-
 function crear_filas(tabla_pasada,elemento,columnas){
 var i=0;
 var celda = [];
+var boton = document.createElement("input");
+boton.setAttribute('name', "eliminar");
 var tabla = document.getElementById(tabla_pasada);
 var fila = tabla.insertRow(-1);
-//se crea el boton
-var boton = document.createElement("input");
-boton.setAttribute('onClick',"eliminar_filas("+tabla_pasada+","+tabla.rows+","+columnas+")"); 
-boton.setAttribute('name', "boton");
-
 /* 	
 	var celda1 = fila.insertCell(0);
 	var celda2 = fila.insertCell(1);
@@ -63,31 +30,22 @@ boton.setAttribute('name', "boton");
 	celda[i] = fila.insertCell(i);
 	celda[i].innerHTML = elemento[elemento.length-columnas+i];
 	}
-
-
-
+	celda[columnas] = fila.insertCell(i);
 	
 }
-
-function eliminar_filas(tabla_pasada,fila,columnas){
-	
-	
-	}
-
-
 </script>
 
 <html>  
 <head>  
 <TITLE>Listado de productos</TITLE> 
-<link rel="stylesheet" href="../styles/login.css"> 
+<link rel="stylesheet" href="../styles/layout.css"> 
 </head>  
 
 <body>  
 
 <div align='left'> 
 	<h1>CARRO DE LA COMPRA</h1>
-	<table id="tabla_carro" border='1' cellpadding='0' cellspacing='0' width='300' bgcolor='#004C80' bordercolor='#1A3300'>  
+	<table id="tabla_carro" border='1' cellpadding='0' cellspacing='0' width='300' bgcolor='#004C80' bordercolor='#1A3300' align="left">  
 	<tr>
 	<td width='150'align="center" style='font-weight: bold'>Id Producto</td> 
 	<td width='150' align="center" style='font-weight: bold'>Precio</td>
@@ -101,7 +59,7 @@ function eliminar_filas(tabla_pasada,fila,columnas){
 
 <div align='center'>  
 	<a href="../index.html">VOLVER A INICIO</a>
-  <table border='1' cellpadding='0' cellspacing='0' width='600' bgcolor='#F6F6F6' bordercolor='#FFFFFF'>  
+  <table border='1' cellpadding='0' cellspacing='0' width='600' bgcolor='#F6F6F6' bordercolor='#FFFFFF' align="center">  
     <tr>  
 	  <td width='150' align="center" style='font-weight: bold'>Caratula</td> 	
       <td width='150' align="center" style='font-weight: bold'>Id Producto</td> 
@@ -117,7 +75,6 @@ function eliminar_filas(tabla_pasada,fila,columnas){
 
 
 <?php
-
    // Definimos los parámetros
    $hostname = "localhost";
    $usuario = "pma";
@@ -138,15 +95,10 @@ function eliminar_filas(tabla_pasada,fila,columnas){
 	$stmt = $conexion->prepare("SELECT caratula,Id_Producto,nombre,Cantidad,precio FROM $tabla ORDER BY Id_Producto");
 	$stmt->execute();
 	$stmt->bind_result($Caratula,$Id_Producto,$Nombre,$Cantidad,$Precio);
-
 	if ($stmt->fetch())
    {
-
-
 	// Mostramos una tabla con el resultado de la consulta
-
 		do {	
-
 		if($Cantidad>0){
 			echo "<tr>";
 				?><td><IMG <?php echo 'SRC="..'.$Caratula.'"' ?> WIDTH="50" HEIGHT="80"></td><?php
@@ -155,7 +107,7 @@ function eliminar_filas(tabla_pasada,fila,columnas){
 				<td>".$Cantidad."</td>			
 				<td>".$Precio."</td>";
 				?><td> <IMG SRC="../images/tick.png" WIDTH="15" HEIGHT="15"></td>
-				<td><button><img src="../images/carrito.png" width="15" height="15" <?php echo 'onclick="carrito('.$Id_Producto.')"' ;?>/></button></td>
+				<td><button><img src="../images/carrito.png" width="15" height="15" <?php echo 'onclick="carrito('.$Id_Producto.','.$Precio.')"' ;?>/></button></td>
 				<?php
 			echo "</tr>";
 				}
@@ -172,9 +124,7 @@ function eliminar_filas(tabla_pasada,fila,columnas){
 			echo "</tr>";
 				}
 			}while ($stmt->fetch());
-
 		
-
 			
 		echo "</table>";
 	}
