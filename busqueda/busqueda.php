f3277fb5692d4e1f2e11802837e4d7819a05fa78
franchojavilondo <franchojavilondo@gmail.com>
2014-11-12 18:03:31 +0100

    mejoras en el carro, ahora se usa jquery

--- busqueda/busqueda.php
+++ busqueda/busqueda.php
@@ -1,39 +1,14 @@
-<script type="text/javascript">
-var carro = [];
-function carrito(x,precio){
-var cadena="Articulos del carrito:\n",i;
-alert("Articulo metido al carrito");
-carro.push(x);
-carro.push(precio);
-for(i=0;i<carro.length;i++){
-cadena+="ID: "+carro[i]+" PRECIO: "+carro[i+1]+"\n";
-i++;
-}
-alert(cadena);
-var columnas=2;
-crear_filas("tabla_carro",carro,columnas)
-}
-function crear_filas(tabla_pasada,elemento,columnas){
-var i=0;
-var celda = [];
-var boton = document.createElement("input");
-boton.setAttribute('name', "eliminar");
-var tabla = document.getElementById(tabla_pasada);
-var fila = tabla.insertRow(-1);
-/* 	
-	var celda1 = fila.insertCell(0);
-	var celda2 = fila.insertCell(1);
-	celda1.innerHTML = elemento[elemento.length-2];
-	celda2.innerHTML = elemento[elemento.length-1]; */
-	
-	for(i=0;i<columnas;i++){
-	celda[i] = fila.insertCell(i);
-	celda[i].innerHTML = elemento[elemento.length-columnas+i];
-	}
-	celda[columnas] = fila.insertCell(i);
-	
-}
-</script>
+<?php
+
+// If your page calls session_start() be sure to include jcart.php first
+include_once('../jcart/jcart.php');
+
+?>
+<link rel="stylesheet" type="text/css" media="screen, projection" href="../jcart/css/jcart.css" />
+
+<script type="text/javascript" src="../jcart/js/jquery-1.4.4.min.js"></script>
+<script type="text/javascript" src="../jcart/js/jcart.min.js"></script>
+<script type="text/javascript" src="../jcart/js/jcart.js"></script>
 
 <html>  
 <head>  
@@ -43,37 +18,11 @@ var fila = tabla.insertRow(-1);
 
 <body>  
 
-<div align='left'> 
-	<h1>CARRO DE LA COMPRA</h1>
-	<table id="tabla_carro" border='1' cellpadding='0' cellspacing='0' width='300' bgcolor='#004C80' bordercolor='#1A3300' align="left">  
-	<tr>
-	<td width='150'align="center" style='font-weight: bold'>Id Producto</td> 
-	<td width='150' align="center" style='font-weight: bold'>Precio</td>
-	<td width='150' align="center" style='font-weight: bold'>Eliminar</td>
-	</tr>
-	
-	
-</table>
+<div align="left" id="jcart"><?php $jcart->display_cart();?></div>
 
-</div>
-
-<div align='center'>  
+<form method="post" action="" class="jcart">  
 	<a href="../index.html">VOLVER A INICIO</a>
-  <table border='1' cellpadding='0' cellspacing='0' width='600' bgcolor='#F6F6F6' bordercolor='#FFFFFF' align="center">  
-    <tr>  
-	  <td width='150' align="center" style='font-weight: bold'>Caratula</td> 	
-      <td width='150' align="center" style='font-weight: bold'>Id Producto</td> 
-	  <td width='150' align="center" style='font-weight: bold'>Nombre</td> 
-	  <td width='150' align="center" style='font-weight: bold'>Cantidad</td> 	  
-      <td width='150' align="center" style='font-weight: bold'>Precio</td> 
-	  <td width='150' align="center" style='font-weight: bold'>Estado</td> 
-	  <td width='150' align="center" style='font-weight: bold'>Sumar al Carrito</td>
-  
-		
-    </tr>  
-
-
-
+<fieldset>
 <?php
    // Definimos los parámetros
    $hostname = "localhost";
@@ -100,33 +49,29 @@ var fila = tabla.insertRow(-1);
 	// Mostramos una tabla con el resultado de la consulta
 		do {	
 		if($Cantidad>0){
-			echo "<tr>";
-				?><td><IMG <?php echo 'SRC="..'.$Caratula.'"' ?> WIDTH="50" HEIGHT="80"></td><?php
-		  echo "<td> ".$Id_Producto."</td>
-				<td>".$Nombre."</td>
-				<td>".$Cantidad."</td>			
-				<td>".$Precio."</td>";
-				?><td> <IMG SRC="../images/tick.png" WIDTH="15" HEIGHT="15"></td>
-				<td><button><img src="../images/carrito.png" width="15" height="15" <?php echo 'onclick="carrito('.$Id_Producto.','.$Precio.')"' ;?>/></button></td>
+				?>
+                <input type="image" name="my-item-caratula" <?php echo 'src="..'.$Caratula.'"' ?> WIDTH="50" HEIGHT="80"> </input>
+                <input type="text" name="my-item-id" <?php echo 'value="'.$Id_Producto.'"' ?>/>
+				<input type="text" name="my-item-name" <?php echo 'value="'.$Nombre.'"' ?>/>
+    			<input type="text" name="my-item-price" <?php echo 'value="'.$Cantidad.'"' ?>/>
+    			<input type="text" name="my-item-url" <?php echo 'value="'.$Precio.'"' ?> />
+				<input type="image" src="../images/tick.png" WIDTH="15" HEIGHT="15"> </input>
 				<?php
-			echo "</tr>";
+
 				}
 		if($Cantidad==0){
-			echo "<tr>";
-				?><td><IMG <?php echo 'SRC="..'.$Caratula.'"' ?> WIDTH="50" HEIGHT="80"></td><?php
-		  echo  "<td>".$Id_Producto."</td>
-				<td>".$Nombre."</td>
-				<td>".$Cantidad."</td>			
-				<td>".$Precio."</td>";
-				?><td> <IMG SRC="../images/cross.png" WIDTH="15" HEIGHT="15"></td>
-
+			?>
+            	<input type="image" name="my-item-caratula" <?php echo 'src="..'.$Caratula.'"' ?> WIDTH="50" HEIGHT="80"> </input>
+                <input type="text" name="my-item-id" <?php echo 'value="'.$Id_Producto.'"' ?>/>
+				<input type="text" name="my-item-name" <?php echo 'value="'.$Nombre.'"' ?>/>
+    			<input type="text" name="my-item-price" <?php echo 'value="'.$Cantidad.'"' ?>/>
+    			<input type="text" name="my-item-url" <?php echo 'value="'.$Precio.'"' ?> />
+				<input type="image" src="../images/cross.png" WIDTH="15" HEIGHT="15"> </input>
 				<?php
-			echo "</tr>";
 				}
 			}while ($stmt->fetch());
 		
 			
-		echo "</table>";
 	}
 	else echo "No hay productos!";
 	//CERRAMOS LA CONSULTA
@@ -136,8 +81,8 @@ var fila = tabla.insertRow(-1);
    
 ?>
 
-   </table>  
-</div>  
+
+</form>  
 </body>  
 
 </html> 
